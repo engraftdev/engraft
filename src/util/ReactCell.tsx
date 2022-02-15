bec4ec83ebad0c64aa0bd2d714856e04dc0a249e
Joshua Horowitz <joshuah@alum.mit.edu>
2022-02-14 17:21:39 -0800

    bring in ReactCell for future reference

---
+++ src/util/ReactCell.tsx
@@ -0,0 +1,33 @@
+import { ReactElement } from 'react';
+import TestRenderer from 'react-test-renderer';
+
+
+export default class ReactCell {
+  testRenderer: TestRenderer.ReactTestRenderer | undefined;
+
+  update(element: ReactElement) {
+    TestRenderer.act(() => {  // TODO: is this helpful?
+      if (!this.testRenderer) {
+        this.testRenderer = TestRenderer.create(element);
+      } else {
+        this.testRenderer.update(element);
+      }
+    });
+  }
+
+  destroy() {
+    if (this.testRenderer) {
+      this.testRenderer.unmount();
+    }
+  }
+}
+
+// if u ever wanna play with intrinsic elements...
+
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       foo: any;
+//     }
+//   }
+// }
