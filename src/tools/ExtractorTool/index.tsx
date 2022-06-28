59e2ae70a74c32d75699ab90fc0a2946733d354d
Joshua Horowitz <joshuah@alum.mit.edu>
2022-06-28 12:59:31 -0700

    refactor ExtractorTool into folder

--- src/tools/ExtractorTool/index.tsx
+++ src/tools/ExtractorTool/index.tsx
@@ -1,150 +1,16 @@
-import { InternMap } from "internmap";
 import React, { createContext, memo, MouseEvent, ReactNode, useCallback, useContext, useEffect, useMemo, useState } from "react";
-import { registerTool, ToolConfig, ToolProps, ToolValue, ToolView, ToolViewProps } from "../tools-framework/tools";
-import { ShowView, useOutput, useSubTool, useView } from "../tools-framework/useSubTool";
-import { newId } from "../util/id";
-import { mapUpdate } from "../util/mapUpdate";
-import { RowToCol } from "../util/RowToCol";
-import { useAt } from "../util/state";
-import { Use } from "../util/Use";
-import { useWindowEventListener } from "../util/useEventListener";
-import useHover from "../util/useHover";
-import { useKeyHeld } from "../util/useKeyHeld";
-import { pathString, ValueCustomizations, ValueOfTool } from "../view/Value";
-import { codeConfigSetTo } from "./CodeTool";
-
-const wildcard = {wildcard: true};
-
-type PatternStep = string | number | typeof wildcard;
-type Pattern = PatternStep[];
-type Path = (string | number)[];
-
-function isWildcard(step: PatternStep): step is typeof wildcard {
-  return typeof step === 'object';
-}
-
-function followPattern(value: any, pattern: Pattern): any {
-  if (pattern.length === 0 || value === undefined) {
-    return value;
-  }
-
-  const [firstStep, ...restPath] = pattern;
-  if (isWildcard(firstStep)) {
-    return Object.values(value).flatMap(subvalue => followPattern(subvalue, restPath));
-  } else {
-    return followPattern(value[firstStep], restPath);
-  }
-}
-
-function mergePatterns(patterns: Pattern[]): (value: unknown) => unknown {
-  const headPaths = patterns.map(getHeadPath);
-  // A pattern with a given head-path will be followed after that head by either:
-  // * the end of the pattern, or
-  // * a wildcard.
-  // If all the patterns here have one or the other of these, life is easy.
-  // Otherwise, we need to append _ROOT & _ALL (names TBD).
-
-  const commonHead = getCommonHead(headPaths);
-
-  const patternsByHead = new InternMap<Path, {atRoot: boolean, afterWildcard: Pattern[]}>([], JSON.stringify);
-
-  patterns.forEach((pattern) => {
-    const headPath = getHeadPath(pattern)
-    const pathAfterCommon = headPath.slice(commonHead.length);
-    mapUpdate(patternsByHead, pathAfterCommon, (prev) => {
-      if (!prev) {
-        prev = {atRoot: false, afterWildcard: []};
-      }
-      if (pattern[headPath.length] === undefined) {
-        prev.atRoot = true;
-      } else if (isWildcard(pattern[headPath.length])) {
-        prev.afterWildcard.push(pattern.slice(headPath.length + 1));
-      } else {
-        throw new Error('waaaaat');
-      }
-      return prev;
-    })
-  })
-
-  let subMergers: {key: string, pathAfterCommon: Path, func: (value: unknown) => unknown}[] = [];
-
-  patternsByHead.forEach(({atRoot, afterWildcard}, pathAfterCommon) => {
-    let conflict = atRoot && afterWildcard.length > 0;
-
-    if (atRoot) {
-      let pathAfterCommonForKey = pathAfterCommon;
-      if (conflict) {
-        pathAfterCommonForKey = [...pathAfterCommon, 'ROOT'];
-      }
-      subMergers.push({
-        key: pathAfterCommonForKey.length === 0 ? 'ROOT' : pathAfterCommonForKey.join('_'),
-        pathAfterCommon,
-        func: (value) => value,
-      });
-    }
-
-    if (afterWildcard.length > 0) {
-      let pathAfterCommonForKey = pathAfterCommon;
-      if (conflict) {
-        pathAfterCommonForKey = [...pathAfterCommon, 'ALL'];
-      }
-
-      const itemFunc = mergePatterns(afterWildcard)
-
-      subMergers.push({
-        key: pathAfterCommonForKey.length === 0 ? 'ALL' : pathAfterCommonForKey.join('_'),
-        pathAfterCommon,
-        func: (value) => typeof value === 'object' && value !== null ? Object.values(value).map(itemFunc) : [],
-      });
-    }
-
-  })
-
-  if (subMergers.length === 1) {
-    if (subMergers[0].pathAfterCommon.length > 0) {
-      throw new Error('no way');
-    }
-    return (value) => {
-      const valueToCommon = followPattern(value, commonHead);
-      return subMergers[0].func(valueToCommon);
-    }
-  } else {
-    return (value) => {
-      const valueToCommon = followPattern(value, commonHead);
-      return Object.fromEntries(subMergers.map(({key, pathAfterCommon, func}) => [
-        key,
-        func(followPattern(valueToCommon, pathAfterCommon))
-      ]))
-    }
-  }
-}
-
-function getHeadPath(pattern: Pattern): Path {
-  let path: Path = [];
-  for (const step of pattern) {
-    if (isWildcard(step)) {
-      return path;
-    }
-    path.push(step);
-  }
-  return path;
-}
-
-function getCommonHead(paths: Path[]): Path {
-  if (paths.length === 0) {
-    throw new Error('idk man');
-  }
-  const [firstPath, ...otherPaths] = paths;
-
-  let common: Path = [];
-  for (let i = 0; i < firstPath.length; i++) {
-    if (otherPaths.some(otherPath => otherPath[i] !== firstPath[i])) {
-      break;
-    }
-    common.push(firstPath[i]);
-  }
-  return common;
-}
+import { registerTool, ToolConfig, ToolProps, ToolValue, ToolView, ToolViewProps } from "src/tools-framework/tools";
+import { ShowView, useOutput, useSubTool, useView } from "src/tools-framework/useSubTool";
+import { newId } from "src/util/id";
+import { RowToCol } from "src/util/RowToCol";
+import { useAt } from "src/util/state";
+import { Use } from "src/util/Use";
+import { useWindowEventListener } from "src/util/useEventListener";
+import useHover from "src/util/useHover";
+import { useKeyHeld } from "src/util/useKeyHeld";
+import { pathString, ValueCustomizations, ValueOfTool } from "src/view/Value";
+import { codeConfigSetTo } from "src/tools/CodeTool";
+import { isWildcard, mergePatterns, Path, Pattern, wildcard } from "./patterns";
 
 interface PatternWithId {
   id: string;
