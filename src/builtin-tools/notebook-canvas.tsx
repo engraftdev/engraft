engraftdev/engraft

commit 35ef09b28e68d68fbf0b99f5d447877dab876c27

notebook-canvas doing good

src/builtin-tools/notebook-canvas.tsx
@@ -1,22 +1,20 @@
+import update from "immutability-helper";
 import _ from "lodash";
 import { memo, useCallback, useEffect, useMemo } from "react";
-import { EnvContext, hasValue, newVar, PossibleEnvContext, PossibleVarBinding, ProgramFactory, ToolOutput, ToolProgram, ToolProps, ToolView, Var, VarBinding } from "src/tools-framework/tools";
+import { hasValue, newVar, PossibleVarBinding, PossibleVarBindingsContext, ProgramFactory, ToolOutput, ToolProgram, ToolProps, ToolView, Var, VarBinding, VarBindingsContext } from "src/tools-framework/tools";
 import { ShowView, useOutput, useTool, useView } from "src/tools-framework/useSubTool";
 import { AddObjToContext } from "src/util/context";
+import { startDrag } from "src/util/drag";
 import { at, atIndex, updateKeys, Updater, useAt, useAtIndex, useStateUpdateOnly } from "src/util/state";
-import { Use } from "src/util/Use";
+import { updateF } from "src/util/updateF";
 import { useContextMenu } from "src/util/useContextMenu";
 import { objEqWith, refEq, useDedupe } from "src/util/useDedupe";
-import useHover from "src/util/useHover";
 import { MyContextMenu, MyContextMenuHeading } from "src/view/MyContextMenu";
-import { Pane, PaneGeo, roundTo } from "src/view/noodle-canvas/model";
+import { PaneGeo, roundTo } from "src/view/noodle-canvas/model";
 import { NoodleCanvas } from "src/view/noodle-canvas/NoodleCanvas";
 import { ToolOutputView } from "src/view/Value";
 import { VarDefinition } from "src/view/Vars";
 import { codeProgramSetTo } from "./code";
-import update from "immutability-helper";
-import { startDrag } from "src/util/drag";
-import { updateF } from "src/util/updateF";
 
 
 export type Program = {
@@ -168,29 +166,7 @@ const View = memo((props: ViewProps) => {
         panes={cells.map((cell, i) => ({
           id: cell.var_.id,
           geo: cell.geo,
-          heading:
-            <div
-              className="NotebookCanvasTool-heading"
-              style={{
-                height: '100%',
-                paddingLeft: '0.5rem',
-                paddingRight: '0.5rem',
-                display: 'flex',
-                alignItems: 'center',
-              }}
-              onContextMenu={}
-            >
-              <div
-                style={{display: 'inline-block', cursor: 'initial'}}
-                onMouseDown={(ev) => ev.stopPropagation()}
-              >
-                <VarDefinition
-                  var_={cell.var_}
-                  updateVar={at(atIndex<Cell>(updateCells, i), 'var_')}
-                />
-              </div>
-            </div>,
-          children:
+          children: ({onMouseDownDragPane}) =>
             <CellView cell={cell}
               // TODO: memoize these?
               updateCell={atIndex(updateCells, i)}
@@ -200,6 +176,7 @@ const View = memo((props: ViewProps) => {
                 updateCells(() => newCells);
               }}
               toolOutput={outputs[cell.var_.id]} toolView={views[cell.var_.id]}
+              onMouseDownDragPane={onMouseDownDragPane}
             />,
         }))}
         updatePaneGeoById={updatePaneGeoById}
@@ -291,8 +268,8 @@ const CellModel = memo(function CellModel({id, cells, updateCells, outputs, repo
   }, [cell, cells, updateCell]), objEqWith(objEqWith(refEq)))
 
 
-  return <AddObjToContext context={EnvContext} obj={newVarBindings}>
-    <AddObjToContext context={PossibleEnvContext} obj={newPossibleVarBindings}>
+  return <AddObjToContext context={VarBindingsContext} obj={newVarBindings}>
+    <AddObjToContext context={PossibleVarBindingsContext} obj={newPossibleVarBindings}>
       {component}
     </AddObjToContext>
   </AddObjToContext>;
@@ -308,11 +285,13 @@ interface CellViewProps {
   toolOutput: ToolOutput | null,
   toolView: ToolView | null,
 
-  removeCell: () => void;
+  removeCell: () => void,
+
+  onMouseDownDragPane: (startEvent: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
 }
 
 const CellView = memo(function CellView(props: CellViewProps) {
-  const {cell, updateCell, toolView, toolOutput, removeCell} = props;
+  const {cell, updateCell, toolView, toolOutput, removeCell, onMouseDownDragPane} = props;
 
   const [var_, updateVar] = useAt(cell, updateCell, 'var_');
 
@@ -332,13 +311,43 @@ const CellView = memo(function CellView(props: CellViewProps) {
     </MyContextMenu>
   , [removeCell]));
 
-  return <div className="NotebookCanvasTool-CellView" onContextMenu={openMenu} style={{height: '100%'}}>
+  return <div className="NotebookCanvasTool-CellView xCol" onContextMenu={openMenu} style={{height: '100%'}}>
     {menuNode}
-    <div className="NotebookCanvasTool-CellView-tool-cell xCol" style={{background: 'rgb(251, 251, 251)'}}>
+    <div
+      className="PaneView-heading"
+      style={{
+        height: '2rem',
+        backgroundColor: 'rgb(240, 240, 240)',
+        borderRadius: '0.25rem 0.25rem 0 0',
+        cursor: 'grab',
+        flexShrink: 0,
+      }}
+      onMouseDown={onMouseDownDragPane}
+    >
+      <div
+        className="NotebookCanvasTool-CellView-heading"
+        style={{
+          height: '100%',
+          paddingLeft: '0.5rem',
+          paddingRight: '0.5rem',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{display: 'inline-block', cursor: 'initial'}}
+          onMouseDown={(ev) => ev.stopPropagation()}
+        >
+          <VarDefinition var_={var_} updateVar={updateVar} />
+        </div>
+      </div>
+    </div>
+
+    <div className="NotebookCanvasTool-CellView-tool xCol" style={{background: 'rgb(251, 251, 251)', minHeight: 0}}>
       <ShowView view={toolView} expand={true}/>
     </div>
     { !alreadyDisplayed &&
-      <div className="NotebookCanvasTool-CellView-output-cell" style={{padding: 5}}>
+      <div className="NotebookCanvasTool-CellView-output" style={{padding: 5, overflow: 'scroll', minHeight: 32, flexShrink: 1000000}}>
         <ToolOutputView toolValue={toolOutput}/>
       </div>
     }

src/view/ToolFrame.tsx
@@ -29,7 +29,7 @@ export const ToolFrame = memo(function ToolFrame(props: ToolFrameProps) {
     className={cN("ToolFrame", {xWidthFitContent: !expand})}
     style={{
       border: '1px solid #c4c4ff', position: "relative", display: 'inline-flex', flexDirection: 'column', boxSizing: 'border-box',
-      height: '100%', maxWidth: '100%',
+      height: '100%', maxWidth: '100%', overflow: 'hidden',
     }}
   >
     <Use hook={useHover} children={([hoverRef, isHovered]) =>

src/TestNoodleCanvas.tsx
@@ -1,46 +1,0 @@
-import { memo, useCallback } from 'react';
-import { useStateUpdateOnly } from './util/state';
-import { updateF } from './util/updateF';
-import { Pane, PaneGeo } from './view/noodle-canvas/model';
-import { NoodleCanvas } from './view/noodle-canvas/NoodleCanvas';
-
-export const TestNoodleCanvas = memo(function TestNoodleCanvas() {
-  const [panes, updatePanes] = useStateUpdateOnly<{[id: string]: Pane}>({
-    'a': {
-      id: 'a',
-      geo: {
-        x: 16,
-        y: 16,
-        width: 16 * 12,
-        height: 16 * 6,
-      },
-      heading: 'hi',
-      children: <h1>hi</h1>
-    },
-    'b': {
-      id: 'b',
-      geo: {
-        x: 16 * 5,
-        y: 16 * 4,
-        width: 16 * 12,
-        height: 16 * 4,
-      },
-      heading: 'hello',
-    },
-  });
-
-  const updatePaneGeoById = useCallback((id: string, f: (old: PaneGeo) => PaneGeo) => {
-    updatePanes(updateF({[id]: {geo: f}}));
-  } , [updatePanes]);
-
-  return (
-    <div className="sizer" style={{width: 600, height: 600, position: 'relative'}}>
-      <NoodleCanvas
-        panes={Object.values(panes)}
-        updatePaneGeoById={updatePaneGeoById}
-        minWidth={16 * 12}
-        minHeight={16 * 4}
-      />
-    </div>
-  );
-});

src/index.tsx
@@ -7,7 +7,6 @@ import App from './App';
 // import TestUseLiveTool from './TestUseLiveTool';
 import TestSynonyms from './TestSynonyms';
 // import TestObservableEmbed from './TestObservableEmbed';
-import { TestNoodleCanvas } from './TestNoodleCanvas';
 
 const root = createRoot(document.getElementById('root')!);
 
@@ -17,7 +16,6 @@ root.render(
       <Routes>
         <Route path="/" element={<App/>}/>
         <Route path="/synonyms" element={<TestSynonyms/>}/>
-        <Route path="/noodle-canvas" element={<TestNoodleCanvas/>}/>
       </Routes>
     </HashRouter>
   </React.StrictMode>,

src/view/noodle-canvas/PaneView.tsx
@@ -22,7 +22,7 @@ export const PaneView = memo(function Pane(props: PaneViewProps) {
     updatePaneGeoById(pane.id, f);
   }, [updatePaneGeoById, pane.id]);
 
-  const onMouseDownDrag = useMemo(() => startDrag({
+  const onMouseDownDragPane = useMemo(() => startDrag({
     init() {
       return {startGeo: geoRef.current};
     },
@@ -71,19 +71,6 @@ export const PaneView = memo(function Pane(props: PaneViewProps) {
           }}
         >
           <div
-            className="PaneView-heading"
-            style={{
-              height: '2rem',
-              backgroundColor: 'rgb(240, 240, 240)',
-              borderRadius: '0.25rem 0.25rem 0 0',
-              cursor: 'grab',
-              flexShrink: 0,
-            }}
-            onMouseDown={onMouseDownDrag}
-          >
-            {pane.heading}
-          </div>
-          <div
             className="PaneView-children-wrapper"
             style={{
               // overflow: 'hidden',  // disabled for CodeMirror popups
@@ -91,7 +78,7 @@ export const PaneView = memo(function Pane(props: PaneViewProps) {
               minHeight: 0,
             }}
           >
-            {pane.children}
+            {pane.children({onMouseDownDragPane})}
           </div>
         </div>
       </div>

src/view/noodle-canvas/model.tsx
@@ -10,8 +10,7 @@ export type PaneGeo = {
 export type Pane = {
   id: string,
   geo: PaneGeo,
-  heading?: ReactNode,
-  children?: ReactNode,
+  children: (props: {onMouseDownDragPane: (startEvent: React.MouseEvent<HTMLDivElement, MouseEvent>) => void}) => ReactNode,
 }
 
 export function roundTo(n: number, step: number) {

commit 8dfd4ca4ce81981ca0899ac875039fa835eb8652

restore sticky behavior (broken by overflow hidden in 35ef09b2)

src/builtin-tools/notebook-canvas.tsx
@@ -165,7 +165,10 @@ const View = memo((props: ViewProps) => {
   }, [smallestUnusedLabel, updateCells]);
 
   return (
-    <div className="NotebookCanvasTool" style={{position: 'relative', width: program.width + 1, height: program.height + 1}}>
+    <div
+      className="NotebookCanvasTool"
+      style={{position: 'relative', width: program.width + 1, height: program.height + 1, overflow: 'hidden'}}
+    >
       <NoodleCanvas
         panes={cells.map((cell, i) => ({
           id: cell.var_.id,

src/view/ToolFrame.tsx
@@ -29,7 +29,7 @@ export const ToolFrame = memo(function ToolFrame(props: ToolFrameProps) {
     className={cN("ToolFrame", {xWidthFitContent: !expand})}
     style={{
       border: '1px solid #c4c4ff', position: "relative", display: 'inline-flex', flexDirection: 'column', boxSizing: 'border-box',
-      height: '100%', maxWidth: '100%', overflow: 'hidden',
+      height: '100%', maxWidth: '100%',
     }}
   >
     <Use hook={useHover} children={([hoverRef, isHovered]) => {