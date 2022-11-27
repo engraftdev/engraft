engraftdev/engraft

commit 69f958f8856a0dabe248e1d0a11539b51f67ca71

remove now-unnecessary (?) div from SubValueHandleDefault

src/view/Value.tsx
@@ -67,9 +67,7 @@ export interface SubValueHandleProps {
 }
 
 export const SubValueHandleDefault = memo(function SubValueHandleDefault({path, children}: SubValueHandleProps) {
-  return <div style={{minWidth: 0}}>
-    {children}
-  </div>
+  return <>{children}</>;
 })
 
 export function pathString(path: (string | number)[]) {

commit 31bfdbfcf224eb32df1ccc78f597e831aab49d54

fix hanging-value bug introduced in 69f958f88 by introducing ANOTHER DIV! 🥳

src/view/Value.tsx
@@ -100,9 +100,11 @@ const ValueInternal = memo(function ValueInternal({value, path, prefix, suffix, 
   function wrapInline(children: ReactNode) {
     return <div className='ValueInternal-wrapInline-row xRow' style={{width: '100%'}}>
       {prefix}
-      <customizations.SubValueHandle value={value} path={path}>
-        {children}
-      </customizations.SubValueHandle>
+      <div className='xShrinkable'>
+        <customizations.SubValueHandle value={value} path={path}>
+          {children}
+        </customizations.SubValueHandle>
+      </div>
       {suffix}
     </div>;
   }