c6600912dd16bbb124944cc9d598925f4e15877d
Joshua Horowitz <joshuah@alum.mit.edu>
2022-11-27 19:01:07 -0800

    ScrollShadow: refactor 2

--- src/view/ScrollShadow.tsx
+++ src/view/ScrollShadow.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, HTMLProps, ReactNode, UIEvent, useCallback, useEffect, useState } from "react";
+import { CSSProperties, HTMLProps, ReactNode, UIEvent, useCallback, useEffect, useMemo, useState } from "react";
 
 interface Props extends Omit<HTMLProps<HTMLDivElement>, 'style'> {
   children: ReactNode;
@@ -9,6 +9,14 @@ interface Props extends Omit<HTMLProps<HTMLDivElement>, 'style'> {
   outerStyle?: CSSProperties;
 }
 
+type ScrollInfo = {
+  scrollLeft: number,
+  scrollTop: number,
+  scrollWidth: number,
+  scrollHeight: number,
+  offsetWidth: number,
+  offsetHeight: number,
+}
 
 export default function ScrollShadow(props: Props) {
   const {
@@ -18,40 +26,48 @@ export default function ScrollShadow(props: Props) {
     shadowSpread = 10,
     innerStyle, outerStyle, ...restProps} = props;
   const [div, setDiv] = useState<HTMLDivElement | null>(null);
-  const [boxShadow, setBoxShadow] = useState('')
+  const [scrollInfo, setScrollInfo] = useState<ScrollInfo | null>(null);
 
-  const updateShadows = useCallback((div: HTMLDivElement) => {
+  const updateScrollInfo = useCallback((div: HTMLDivElement) => {
+    const {scrollLeft, scrollTop, scrollWidth, scrollHeight, offsetWidth, offsetHeight} = div;
+    setScrollInfo({scrollLeft, scrollTop, scrollWidth, scrollHeight, offsetWidth, offsetHeight});
+  }, []);
+
+  const onScroll = useCallback((ev: UIEvent<HTMLDivElement>) => {
+    const div = ev.currentTarget;
+    updateScrollInfo(div);
+  }, [updateScrollInfo])
+
+  useEffect(() => {
+    if (div) {
+      const observer = new ResizeObserver(() => updateScrollInfo(div));
+      observer.observe(div);
+      return () => observer.disconnect();
+    }
+  }, [div, updateScrollInfo])
+
+  const boxShadow = useMemo(() => {
+    if (!scrollInfo) { return undefined; }
+    const {scrollLeft, scrollTop, scrollWidth, scrollHeight, offsetWidth, offsetHeight} = scrollInfo;
+
     const boxShadowBits: string[] = [];
     if (innerStyle?.boxShadow) {
       boxShadowBits.push(innerStyle.boxShadow);
     }
-    if (div.scrollTop > 0) {
+    if (scrollTop > 0) {
       boxShadowBits.push(`inset 0 ${shadowBlur + shadowSpread}px ${shadowBlur}px -${shadowBlur}px ${shadowColor}`);
     }
-    if (div.scrollTop < div.scrollHeight - div.offsetHeight - 1) {
+    if (scrollTop < scrollHeight - offsetHeight - 1) {
       boxShadowBits.push(`inset 0 -${shadowBlur + shadowSpread}px ${shadowBlur}px -${shadowBlur}px ${shadowColor}`);
     }
-    if (div.scrollLeft > 0) {
+    if (scrollLeft > 0) {
       boxShadowBits.push(`inset ${shadowBlur + shadowSpread}px 0 ${shadowBlur}px -${shadowBlur}px ${shadowColor}`);
     }
-    if (div.scrollLeft < div.scrollWidth - div.offsetWidth - 1) {
+    if (scrollLeft < scrollWidth - offsetWidth - 1) {
       boxShadowBits.push(`inset -${shadowBlur + shadowSpread}px 0 ${shadowBlur}px -${shadowBlur}px ${shadowColor}`);
     }
-    setBoxShadow(boxShadowBits.join(', '));
-  }, [shadowBlur, shadowColor, shadowSpread, innerStyle?.boxShadow])
-
-  const onScroll = useCallback((ev: UIEvent<HTMLDivElement>) => {
-    const div = ev.currentTarget;
-    updateShadows(div);
-  }, [updateShadows])
-
-  useEffect(() => {
-    if (div) {
-      const observer = new ResizeObserver(() => updateShadows(div));
-      observer.observe(div);
-      return () => observer.disconnect();
-    }
-  }, [div, updateShadows])
+    return boxShadowBits.join(', ');
+  }, [scrollInfo, innerStyle?.boxShadow, shadowColor, shadowBlur, shadowSpread]);
 
   // quick little notes on how this works...
   // * the inner and outer divs should be the same size: small.
