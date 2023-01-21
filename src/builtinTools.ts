c85c443cc404389bae22c6720f3f5afaf3f0ef9f
Joshua Horowitz <joshuah@alum.mit.edu>
2023-01-20 23:05:43 -0800

    all builtinTools in folders

--- src/builtinTools.ts
+++ src/builtinTools.ts
@@ -1,6 +1,6 @@
 import { toolFromModule } from "./toolFromModule";
 
-// this matches "./builtin-tools/some.tsx" or "./builtin-tools/some/index.tsx"
-const toolsContext = require.context('.', true, /^\.\/builtin-tools\/([^/]*)\.[jt]sx?$|^\.\/builtin-tools\/([^/]*)\/index\.tsx$/);
+// this matches "./builtin-tools/[something]/index.tsx"
+const toolsContext = require.context('.', true, /^\.\/builtin-tools\/([^/]*)\/index\.tsx$/);
 
 export const builtinTools = toolsContext.keys().map(id => toolFromModule(toolsContext(id)));
