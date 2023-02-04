8d3d46f5cec179ba3a4ee1c4be820359daf0a121
Joshua Horowitz <joshuah@alum.mit.edu>
2023-02-03 20:02:38 -0800

    setEqWithRefEq + recordEqWith

--- src/util/eq.ts
+++ src/util/eq.ts
@@ -41,3 +41,26 @@ export function arrEqWith<T, U extends (readonly T[]) | T[]>(eq: Eq<T>): Eq<U> {
   }
 }
 export const arrEqWithRefEq = arrEqWith(refEq);
+
+export function setEqWithRefEq<T>(s1: Set<T>, s2: Set<T>): boolean {
+  if (s1.size !== s2.size) { return false; }
+  for (let elem of s1) {
+    if (!s2.has(elem)) {
+      return false;
+    }
+  }
+  return true;
+}
+// note that setEqWith(someOtherEq) is harder to implement & probably quadratic
+
+export function recordEqWith<T>(eqs: { [K in keyof T]: Eq<T[K]> }): Eq<T> {
+  return (o1: T, o2: T) => {
+    if (o1 === o2) { return true; }
+    for (let key in eqs) {
+      if (!eqs[key](o1[key], o2[key])) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
