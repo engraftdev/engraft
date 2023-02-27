01fe1211e30782f287ca10db49b00a1f3164269c
Joshua Horowitz <joshuah@alum.mit.edu>
2023-02-27 12:14:36 -0800

    remove test utilities

--- tests/mockStyle.js
+++
@@ -1,2 +0,0 @@
-// Our components import CSS stylesheets as strings. This emulates that (well enough) for testing.
-module.exports = "";
--- tests/setupFileAfterEnv.js
+++
@@ -1,4 +0,0 @@
-import console from "console"
-
-// Disable Jest's custom console output system
-global.console = console
