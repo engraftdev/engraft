ed8028fe5d7f81f1fd6fa97f47c0053c68bdf2dc
Joshua Horowitz <joshuah@alum.mit.edu>
2023-02-27 20:03:08 -0800

    split off update-proxy[-react]; haven't switched original over

---
+++ packages/update-proxy-react/src/test.tsx
@@ -0,0 +1,68 @@
+import { UpdateProxy, Updater } from '@engraft/update-proxy';
+import { Fragment } from 'react';
+import TestRenderer from 'react-test-renderer';
+import { describe, expect, it } from 'vitest';
+import { UseUpdateProxy, useUpdateProxy } from '../src';
+
+describe('useUpdateProxy', () => {
+  it('basically works', () => {
+    let latestUpdateProxy: UpdateProxy<object> | null = null;
+    const MyComponent = (props: {updater: Updater<object>}) => {
+      latestUpdateProxy = useUpdateProxy(props.updater);
+      return null;
+    };
+
+    const testRenderer = TestRenderer.create(<Fragment></Fragment>);
+
+    const updater1 = () => {};
+    testRenderer.update(<MyComponent updater={updater1} />);
+    expect(latestUpdateProxy).not.toBeNull();
+    const updateProxy1 = latestUpdateProxy!;
+
+    testRenderer.update(<MyComponent updater={updater1} />);
+    expect(latestUpdateProxy).toBe(updateProxy1);
+
+    const updater2 = () => {};
+    testRenderer.update(<MyComponent updater={updater2} />);
+    expect(latestUpdateProxy).not.toBeNull();
+    const updateProxy2 = latestUpdateProxy!;
+    expect(updateProxy2).not.toBe(updateProxy1);
+  });
+});
+
+describe('UseUpdateProxy', () => {
+  it('basically works', () => {
+    let latestUpdateProxy: UpdateProxy<object> | null = null;
+
+    const testRenderer = TestRenderer.create(<Fragment></Fragment>);
+
+    const updater1: Updater<object> = () => {};
+    testRenderer.update(
+      <UseUpdateProxy updater={updater1} children={(updateProxy) => {
+        latestUpdateProxy = updateProxy;
+        return null;
+      }} />
+    );
+    expect(latestUpdateProxy).not.toBeNull();
+    const updateProxy1 = latestUpdateProxy!;
+
+    testRenderer.update(
+      <UseUpdateProxy updater={updater1} children={(updateProxy) => {
+        latestUpdateProxy = updateProxy;
+        return null;
+      }} />
+    );
+    expect(latestUpdateProxy).toBe(updateProxy1);
+
+    const updater2: Updater<Object> = () => {};
+    testRenderer.update(
+      <UseUpdateProxy updater={updater2} children={(updateProxy) => {
+        latestUpdateProxy = updateProxy;
+        return null;
+      }} />
+    );
+    expect(latestUpdateProxy).not.toBeNull();
+    const updateProxy2 = latestUpdateProxy!;
+    expect(updateProxy2).not.toBe(updateProxy1);
+  });
+});
---
+++ packages/update-proxy/src/index.ts
@@ -0,0 +1,123 @@
+import immutabilityHelper, { Spec } from "immutability-helper";
+
+export type UpdateProxy<T> =
+  & {
+      [K in keyof T]-?: Omit<T, K> extends T ? UpdateProxyRemovable<T[K]> : UpdateProxy<T[K]>
+    }
+  & {
+      $: (f: (oldT: T) => T) => void,  // shorthand for $apply
+      $apply: (f: (oldT: T) => T) => void,
+      $set: (newT: T) => void,
+      $helper: (spec: Spec<T>) => void,
+      $as: <U>(u?: U) => UpdateProxy<U>,
+    }
+  & (
+      T extends (infer E)[]
+      ? { $all: UpdateProxy<E> }
+      : {}
+    )
+  & (
+      T extends {[key: string]: infer E}
+      ? { $all: UpdateProxy<E> }
+      : {}
+    );
+
+export type UpdateProxyRemovable<T> = UpdateProxy<T> & {
+  $remove: () => void;
+}
+
+export type Updater<T> = (update: (oldT: T) => T) => void;
+
+export function updateProxy<T>(updater: Updater<T>): UpdateProxy<T>;
+export function updateProxy<T>(updater: Updater<T>, remover: () => void): UpdateProxyRemovable<T>;
+export function updateProxy<T>(updater: Updater<T>, remover?: () => void): UpdateProxy<T> {
+  // Note: Although we want the interface of this function to be safely typed, it is impossible to
+  // make its internals safely typed because of the weakness of Proxy's handler types (etc). So we
+  // won't particularly try.
+
+  const up: UpdateProxy<T> = new Proxy<UpdateProxy<T>>({
+    $: updater,
+    $apply: updater,
+    $set: (newT) => updater(() => newT),
+    $helper: (spec) => updater((oldT) => immutabilityHelper(oldT, spec)),
+    $as: <U extends T>(u?: U) => up as unknown as UpdateProxy<U>,
+    ...remover && {$remove: remover},
+  } as UpdateProxy<T>, {
+    get(target, key) {
+      // Return cached properties
+      if (key in target) {
+        return target[key as keyof UpdateProxy<T>];
+      }
+
+      // Return special lazy properties
+      if (key === '$all') {
+        const propProxy = updateProxy(atAllObjOrArray(updater as any as Updater<T[]>));
+        (target as any).$all = propProxy;
+        return propProxy;
+      }
+
+      // Now we can assume `key` is a key of T and hasn't been cached
+      const keyTyped = key as keyof T;
+      const propProxy = updateProxy(
+        atObjOrArray(updater, keyTyped),
+        // Fun fact: We always provide a remover, even if the property isn't removable.
+        // (No way to tell the difference between essential & inessential properties at runtime!)
+        // The typing of UpdateProxy itself stops the remover from being called on essential properties.
+        removeFromObjOrArray(updater, keyTyped),
+      );
+      target[keyTyped] = propProxy as any;
+      return propProxy;
+    }
+  });
+
+  return up;
+}
+
+function atObjOrArray<T, K extends keyof T>(update: Updater<T>, key: K): Updater<T[K]> {
+  return (f: (oldTK: T[K]) => T[K]) => {
+    update((oldT: T) => {
+      if (oldT instanceof Array) {
+        const newTs = oldT.slice();
+        newTs[key as number] = f(newTs[key as number]);
+        return newTs as T;
+      } else {
+        return {...oldT, [key]: f(oldT[key])};
+      }
+    });
+  };
+}
+
+// this (internal) function is not well typed; it should only be called when `Omit<T, K> extends T`.
+function removeFromObjOrArray<T, K extends keyof T>(update: Updater<T>, key: K): () => void {
+  return () => {
+    update((oldT: T) => {
+      if (oldT instanceof Array) {
+        const newTs = oldT.slice();
+        newTs.splice(key as number, 1);
+        return newTs as T;
+      } else {
+        const newT = {...oldT};
+        delete newT[key];
+        return newT as T;
+      }
+    });
+  };
+}
+
+// this (internal) function is not well typed.
+type ObjOrArrayElement<T> = T extends (infer E)[] ? E : T extends {[key: string]: infer E} ? E : never;
+function atAllObjOrArray<T>(update: Updater<T>): Updater<ObjOrArrayElement<T>> {
+  return (f: (oldTElem: ObjOrArrayElement<T>) => ObjOrArrayElement<T>) => {
+    update((oldT) => {
+      if (oldT instanceof Array) {
+        return oldT.map(f) as T;
+      } else {
+        const result: {[key: string]: ObjOrArrayElement<T>} = {};
+        for (const key in oldT) {
+          result[key] = f(oldT[key] as ObjOrArrayElement<T>);
+        }
+        return result as T;
+      }
+    });
+  };
+}
