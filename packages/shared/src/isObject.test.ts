13fd234a9a0f13ea9c96e3270cfefc954e483e4c
Joshua Horowitz <joshuah@alum.mit.edu>
2023-02-27 23:23:06 -0800

    shared: isObject test

---
+++ packages/shared/src/isObject.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest';
+import { isObject } from './isObject';
+
+describe('isObject', () => {
+  it('basically works', () => {
+    expect(isObject({})).toBe(true);
+    expect(isObject([])).toBe(true);
+    expect(isObject(() => {})).toBe(false);
+    expect(isObject(null)).toBe(false);
+    expect(isObject(undefined)).toBe(false);
+    expect(isObject(123)).toBe(false);
+    expect(isObject('abc')).toBe(false);
+    expect(isObject(true)).toBe(false);
+  });
+});
