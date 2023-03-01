30be461acf4cd2333a731fa28ff978bce83919f5
Joshua Horowitz <joshuah@alum.mit.edu>
2023-03-01 14:47:30 -0800

    github action for deploying testbed which basically works

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { checker } from "vite-plugin-checker";
 
 export default defineConfig({
+  base: './',
   plugins: [
     checker({ typescript: true }),
   ],
