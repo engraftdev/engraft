4ed75a2b0b4f26e8f4f4aa5f758cba11082a245e
Joshua Horowitz <joshuah@alum.mit.edu>
2023-02-28 19:58:48 -0800

    instituted our-depcheck.js as a more dependable alternative to eslint (import/no-extraneous-dependencies)

---
+++ scripts/our-depcheck.js
@@ -0,0 +1,35 @@
+const depcheck = require('depcheck');
+
+const options = {
+  // tests will use devDependencies from the root package.json; ignore them
+  ignorePatterns: ["*.test.ts", "*.test.tsx", "test.ts", "test.tsx"]
+};
+
+depcheck(process.cwd(), options).then((unused) => {
+  let problem = false;
+
+  if (unused.dependencies.length > 0) {
+    console.log("Unused dependencies");
+    for (const dep of unused.dependencies) {
+      console.log(`  ${dep}`);
+    }
+    problem = true;
+  }
+
+  const missing = Object.keys(unused.missing);
+  if (missing.length > 0) {
+    console.log("Missing dependencies");
+    for (const dep of missing) {
+      console.log(`  ${dep}`);
+    }
+    problem = true;
+  }
+
+  // TODO: handle unused.invalidFiles and unused.invalidDirs?
+
+  if (problem) {
+    process.exit(1);
+  } else {
+    console.log("No problems found by our-depcheck.js");
+  }
+});
