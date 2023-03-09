ef4fa8829601df94042d829bf4da6ec24f555d28
Joshua Horowitz <joshuah@alum.mit.edu>
2023-03-09 15:31:19 -0800

    tool-toy-adder: formatting

--- packages/tool-toy-adder/src/index.tsx
+++ packages/tool-toy-adder/src/index.tsx
@@ -18,14 +18,17 @@ const computeReferences: ComputeReferences<Program> = (program) =>
 const run = memoizeProps(hooks((props: ToolProps<Program>) => {
   const { program, varBindings } = props;
 
-  const {outputP: xOutputP, view: xView} = hookRunTool({program: program.xProgram, varBindings});
-  const {outputP: yOutputP, view: yView} = hookRunTool({program: program.yProgram, varBindings});
+  const xResults = hookRunTool({program: program.xProgram, varBindings});
+  const yResults = hookRunTool({program: program.yProgram, varBindings});
 
-  const outputP = hookMemo(() => EngraftPromise.all([xOutputP, yOutputP]).then(([xOutput, yOutput]) => {
-    if (typeof xOutput.value !== 'number') { throw new Error('x must be a number'); }
-    if (typeof yOutput.value !== 'number') { throw new Error('y must be a number'); }
-    return {value: xOutput.value + yOutput.value};
-  }), [xOutputP, yOutputP]);
+  const outputP = hookMemo(() =>
+    EngraftPromise.all([xResults.outputP, yResults.outputP])
+      .then(([xOutput, yOutput]) => {
+        if (typeof xOutput.value !== 'number') { throw new Error('x must be a number'); }
+        if (typeof yOutput.value !== 'number') { throw new Error('y must be a number'); }
+        return {value: xOutput.value + yOutput.value};
+      })
+  , [xResults.outputP, yResults.outputP]);
 
   const view: ToolView<Program> = hookMemo(() => ({
     render: ({updateProgram, autoFocus}) =>
@@ -33,16 +36,23 @@ const run = memoizeProps(hooks((props: ToolProps<Program>) => {
         <div className="xCol xGap10 xPad10">
           <div className="xRow xGap10">
             <b>x</b>
-            <ShowView view={xView} updateProgram={programUP.xProgram.$apply} autoFocus={autoFocus} />
+            <ShowView
+              view={xResults.view}
+              updateProgram={programUP.xProgram.$apply}
+              autoFocus={autoFocus}
+            />
           </div>
 
           <div className="xRow xGap10">
             <b>y</b>
-            <ShowView view={yView} updateProgram={programUP.yProgram.$apply} />
+            <ShowView
+              view={yResults.view}
+              updateProgram={programUP.yProgram.$apply}
+            />
           </div>
         </div>
       } />
-  }), [xView, yView]);
+  }), [xResults.view, yResults.view]);
 
   return {outputP, view};
 }));
