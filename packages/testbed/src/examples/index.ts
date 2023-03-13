89d282f3c5add33a2f846c20f53edbd113f562fd
Joshua Horowitz <joshuah@alum.mit.edu>
2023-03-13 14:04:41 -0700

    testbed: fix `meta` example, fix extra `default` in examples

--- packages/testbed/src/examples/index.ts
+++ packages/testbed/src/examples/index.ts
@@ -4,5 +4,6 @@ const modules = import.meta.glob('./*.json', { eager: true });
 export const examples =
   Object.entries(modules).map(([filename, program]) => ({
     name: filename.match(/\.\/(.*)\.json$/)![1],
-    program: program as ToolProgram
+    // vite seems to import json both directly and with `default`; let's just take default
+    program: (program as {default: ToolProgram}).default,
   }));
