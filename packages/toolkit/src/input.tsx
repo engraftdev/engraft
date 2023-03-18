effe82e312c6038e3695e60b92d5fd515a8d03b2
Joshua Horowitz <joshuah@alum.mit.edu>
2023-03-17 19:43:15 -0700

    toolkit: new input icon

--- packages/toolkit/src/input.tsx
+++ packages/toolkit/src/input.tsx
@@ -14,7 +14,7 @@ export const InputHeading = memo(function InputHeading(props: {
 }) {
   const { slot, headingCommonWidth } = props;
 
-  const icon = <div style={{ width: 20, paddingTop: 3 }}>{inputIcon}</div>;
+  const icon = <div style={{ minWidth: 20, height: 20, paddingTop: 5 }}>{inputIcon}</div>;
 
   return (
     <div
@@ -34,12 +34,10 @@ export const InputHeading = memo(function InputHeading(props: {
 });
 
 export const inputIcon =
-  <svg version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg">
-    <filter id="dilate">
-      <feMorphology operator="dilate" radius="15" />
-    </filter>
-    <path
-      style={{filter: 'url(#dilate)'}}
-      d="m714.64 650h-689.64c-13.809 0-25-11.191-25-25s11.191-25 25-25h689.64l-307.32-307.32c-9.7656-9.7617-9.7656-25.59 0-35.352 9.7617-9.7656 25.59-9.7656 35.352 0l350 350c9.7656 9.7617 9.7656 25.59 0 35.352l-350 350c-9.7617 9.7656-25.59 9.7656-35.352 0-9.7656-9.7617-9.7656-25.59 0-35.352zm235.36-450h-175c-13.809 0-25-11.191-25-25s11.191-25 25-25h200c13.809 0 25 11.191 25 25v900c0 13.809-11.191 25-25 25h-200c-13.809 0-25-11.191-25-25s11.191-25 25-25h175z"
-    />
+  <svg version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g>
+      <path d="m40.289 56.539v10.832h27.082v-27.082h-10.832v16.25z"/>
+      <path d="m9.375 17.035 7.6602-7.6602 48.75 48.75-7.6602 7.6602z"/>
+      <path d="m90.625 90.625h-81.25v-40.625h10.832v29.793h59.586v-59.586h-29.793v-10.832h40.625z"/>
+    </g>
   </svg>;
