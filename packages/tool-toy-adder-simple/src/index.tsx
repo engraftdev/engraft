94acbdd0dadabbd55e690b3caec80758cacd8167
Joshua Horowitz <joshuah@alum.mit.edu>
2023-03-24 17:02:41 -0700

    toy-adder-simple: remove extra field, for parity with toy-adder

--- packages/tool-toy-adder-simple/src/index.tsx
+++ packages/tool-toy-adder-simple/src/index.tsx
@@ -2,34 +2,22 @@ import { defineSimpleTool } from "@engraft/toolkit";
 
 export default defineSimpleTool({
   name: 'toy-adder-simple',
-  fields: {
-    extra: 0,
-  },
+  fields: { },
   subTools: ['x', 'y'],
-  compute: ({ fields, subToolOutputs }) => {
+  compute: ({ subToolOutputs }) => {
     if (typeof subToolOutputs.x !== 'number') { throw new Error('x must be a number'); }
     if (typeof subToolOutputs.y !== 'number') { throw new Error('y must be a number'); }
-    return subToolOutputs.x + subToolOutputs.y + fields.extra;
+    return subToolOutputs.x + subToolOutputs.y;
   },
-  render: ({ renderSlot, autoFocus, fields, fieldsUP }) => (
+  render: ({ renderSlot, autoFocus }) => (
     <div className="xCol xGap10 xPad10">
       <div className="xRow xGap10">
         <b>x</b>
         {renderSlot('x', {autoFocus})}
       </div>
-
       <div className="xRow xGap10">
         <b>y</b>
         {renderSlot('y')}
-      </div>
-
-      <div className="xRow xGap10">
-        <b>extra</b>
-        <input
-          type="range"
-          value={fields.extra}
-          onChange={(e) => fieldsUP.extra.$set(+e.target.value)}
-        />
       </div>
     </div>
   ),
