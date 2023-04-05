dd42f48314ee46bcd69b869847ab1ac8dbf77a4f
Joshua Horowitz <joshuah@alum.mit.edu>
2023-04-05 12:43:20 -0700

    tool-color: fix glitching

--- packages/original/src/builtin-tools/color/index.tsx
+++ packages/original/src/builtin-tools/color/index.tsx
@@ -1,34 +1,25 @@
-import { ComputeReferences, EngraftPromise, ProgramFactory, ToolProps, ToolRun, ToolView } from "@engraft/core";
-import { hookMemo, hooks, memoizeProps } from "@engraft/refunc";
-import { RgbColorPicker } from "react-colorful";
+import { defineSimpleTool } from "@engraft/toolkit";
+import { RgbColor, RgbColorPicker } from "react-colorful";
 
-export type Program = {
-  toolName: 'color',
-  r: number,
-  g: number,
-  b: number,
-}
-
-export const programFactory: ProgramFactory<Program> = () => ({
-  toolName: 'color',
-  r: 250,
-  g: 200,
-  b: 100,
+export default defineSimpleTool({
+  name: 'color',
+  fields: {
+    r: 250,
+    g: 200,
+    b: 100,
+  },
+  subTools: [],
+  compute: ({ fields }) => {
+    return `rgb(${fields.r}, ${fields.g}, ${fields.b})`;
+  },
+  render: ({ fields, fieldsUP }) => {
+    const onChange = (color: RgbColor) => {
+      fieldsUP.$helper({$merge: color});
+    };
+    return <RgbColorPicker
+      color={fields}
+      onChange={onChange}
+      style={{width: 150, height: 150}}
+    />;
+  },
 });
-
-export const computeReferences: ComputeReferences<Program> = (program) => new Set();
-
-export const run: ToolRun<Program> = memoizeProps(hooks((props: ToolProps<Program>) => {
-  const { program } = props;
-
-  const outputP = hookMemo(() => EngraftPromise.resolve({
-    value: `rgb(${program.r}, ${program.g}, ${program.b})`
-  }), [program]);
-
-  const view: ToolView<Program> = hookMemo(() => ({
-    render: ({updateProgram}) =>
-      <RgbColorPicker color={program} onChange={(color) => updateProgram((old) => ({...old, ...color}))} style={{width: 150, height: 150}}/>
-  }), [program]);
-
-  return { outputP, view };
-}));
