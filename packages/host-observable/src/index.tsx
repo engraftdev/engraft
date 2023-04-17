9c76f95f2f819a272f527bdee9b0cce686f00551
Joshua Horowitz <joshuah@alum.mit.edu>
2023-04-17 16:12:31 -0700

    updates after rebase

--- packages/host-observable/src/index.tsx
+++ packages/host-observable/src/index.tsx
@@ -3,7 +3,7 @@ import IsolateStyles from '@engraft/original/lib/view/IsolateStyles.js';
 import { ToolOutputBuffer } from '@engraft/original/lib/view/Value.js';
 import { ErrorBoundary } from '@engraft/original/lib/util/ErrorBoundary.js';
 import { isObject } from '@engraft/shared/lib/isObject.js';
-import { EngraftPromise, PromiseState, runTool, ShowView, slotWithCode, ToolOutput, ToolProgram, useIncr, usePromiseState, VarBinding } from '@engraft/toolkit';
+import { EngraftPromise, PromiseState, runTool, ShowView, slotWithCode, ToolOutput, ToolProgram, usePromiseState, useRefunction, VarBinding } from '@engraft/toolkit';
 import { isValidElement, memo, useEffect, useMemo, useState } from 'react';
 import React from 'react';
 import { registerAllTheTools } from '@engraft/all-the-tools';
@@ -18,25 +18,25 @@ type ObservableEmbedProps = {
   localStorageKey: string,
   reportOutputState?: (outputState: PromiseState<ToolOutput>) => void,
   reportOutputP?: (outputState: EngraftPromise<ToolOutput>) => void,
-  variables: {[name: string]: any} | undefined,
+  inputs: {[name: string]: any} | undefined,
   hide?: boolean,
 }
 
 export const ObservableEmbed = memo(function ObservableEmbed(props: ObservableEmbedProps) {
-  const { localStorageKey, variables = {}, reportOutputState, reportOutputP, hide = false } = props;
+  const { localStorageKey, inputs = {}, reportOutputState, reportOutputP, hide = false } = props;
 
-  // TODO: useEngraft does this too
+  // turn inputs provided from Observable into varBindings
   const varBindings = useMemo(() => {
     let varBindings: {[id: string]: VarBinding} = {};
-    Object.entries(variables || {}).forEach(([name, value]) => {
+    Object.entries(inputs || {}).forEach(([name, value]) => {
       const id = `ID${name}000000`;
       varBindings[id] = {var_: {id, label: name}, outputP: EngraftPromise.resolve({value: value})};
     });
     return varBindings;
-  }, [variables]);
+  }, [inputs]);
 
   const [program, updateProgram] = useState<ToolProgram>(
-    programFromLocalStorage(localStorageKey) || slotWithCode(defaultCodeFromVariables(variables))
+    programFromLocalStorage(localStorageKey) || slotWithCode(defaultCodeFromInputs(inputs))
   );
 
   useEffect(() => {
@@ -47,7 +47,7 @@ export const ObservableEmbed = memo(function ObservableEmbed(props: ObservableEm
     }
   }, [program, localStorageKey])
 
-  const {outputP, view} = useIncr(runTool, { program, varBindings });
+  const {outputP, view} = useRefunction(runTool, { program, varBindings });
 
   useEffect(() => {
     reportOutputP && reportOutputP(outputP);
@@ -83,8 +83,8 @@ export const ObservableEmbed = memo(function ObservableEmbed(props: ObservableEm
 });
 
 // TODO: useEngraft does this too
-function defaultCodeFromVariables(variables: {[name: string]: any}) {
-  const names = Object.keys(variables);
+function defaultCodeFromInputs(inputs: {[name: string]: any}) {
+  const names = Object.keys(inputs);
   const firstName: string | undefined = names[0];
   if (firstName) {
     return `ID${firstName}000000`;
@@ -93,7 +93,6 @@ function defaultCodeFromVariables(variables: {[name: string]: any}) {
   }
 }
 
-// TODO: useEngraft does this too
 function programFromLocalStorage(key: string) {
   const programJson = window.localStorage.getItem(key)
   if (programJson) {
