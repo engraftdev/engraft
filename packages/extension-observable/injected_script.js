3adab09787678211b4028362a6a517a5afaabea9
hao <wongyh@uw.edu>
2023-04-24 15:05:13 -0700

    string replacement works with hardcoded cell-order

--- packages/extension-observable/injected_script.js
+++ packages/extension-observable/injected_script.js
@@ -22,14 +22,6 @@ function getText(view) {
   return view?.editorView.state.doc.toString();
 }
 
-function replaceProgramString(editorView, programString) {
-  // programString = latest program string transmitted from a updateProgram event from host
-  const re = /(?<=engraft\('\w+', {.*?}, this, ).*?(?=\))/
-  const oldString = "" // get from editor view
-  const newString = oldString.replace(re, programString)
-  editorView.dispatch({changes: { from: 0, to: editorView.state.doc.length, insert: newString}});
-}
-
 window.addEventListener("message", (event) => {
   if (event.data !== null && typeof event.data === "object" && event.data.source === "observable-writer") {
     console.log("parent got event", event.data);
@@ -40,11 +32,15 @@ window.addEventListener("message", (event) => {
       replaceText(view.editorView, "WOW");
     }
     if (event.data.type === 'engraft-update') {
-      const { order, program} = event.data;
+      const {order, program} = event.data;
       const view = getView(getCell(order));
       console.log('updating engraft in cell ', order)
 
-      replaceProgramString(view.editorView, program)
+      const re = /(?<=engraft\('\w+', {.*?}, this, ).*?[\s]*(?=\))/
+      const oldString = view.editorView.state.doc.toString()
+      const newString = oldString.replace(re, JSON.stringify(program))
+
+      replaceText(view.editorView, newString)
     }
   }
 });
