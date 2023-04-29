5426d92a0ef6ddc7b9c517aa1083ff9b8e54bc3a
Joshua Horowitz <joshuah@alum.mit.edu>
2023-04-28 19:46:12 -0700

    graft-garden: numerous improvements

--- packages/graft-garden/src/index.tsx
+++ packages/graft-garden/src/index.tsx
@@ -1,7 +1,7 @@
 import { registerAllTheTools } from "@engraft/all-the-tools";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes, useParams } from "react-router-dom";
 import { EditPatch } from "./EditPatch.js";
 import { Root } from "./Root.js";
 import { TinkerPatch } from "./TinkerPatch.js";
@@ -18,11 +18,31 @@ root.render(
     <HashRouter>
       <Routes>
         <Route path="/" element={<Root/>}/>
-        <Route path="/edit/:patchId" element={<EditPatch/>}/>
-        <Route path="/edit/:patchId/safe" element={<EditPatch safeMode/>}/>
-        <Route path="/view/:patchId" element={<ViewPatch/>}/>
-        <Route path="/tinker/:patchId" element={<TinkerPatch/>}/>
+        <Route path="/:patchId">
+          <Route path="edit" element={<EditPatch/>}/>
+          <Route path="edit/safe" element={<EditPatch safeMode/>}/>
+          <Route path="view" element={<ViewPatch/>}/>
+          <Route path="tinker" element={<TinkerPatch/>}/>
+        </Route>
+        {/* deprecated routes; redirect */}
+        <Route path="/edit/:patchId" element={<Redirect to="/:patchId/edit"/>}/>
+        <Route path="/edit/:patchId/safe" element={<Redirect to="/:patchId/edit/safe"/>}/>
+        <Route path="/view/:patchId" element={<Redirect to="/:patchId/view"/>}/>
+        <Route path="/tinker/:patchId" element={<Redirect to="/:patchId/tinker"/>}/>
       </Routes>
     </HashRouter>
   </React.StrictMode>,
 );
+
+function Redirect({ to }: { to: string }) {
+  const params = useParams();
+  for (const key in params) {
+    const value = params[key];
+    if (!value) {
+      throw new Error(`Redirect: missing param ${key}`);
+    }
+    to = to.replace(`:${key}`, value);
+  }
+  console.warn(`Redirecting from deprecated route: ${window.location.hash.slice(1)} -> ${to}`);
+  return <Navigate to={to} replace />;
+}
