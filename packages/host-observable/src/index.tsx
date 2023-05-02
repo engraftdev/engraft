276c2a0e04184c9f309b3f3d8dd8e991ad0091aa
Yu Hao Wong <54234367+wongyuhao@users.noreply.github.com>
2023-05-01 17:32:44 -0700

    depreceate localstorage based extension ObservableExtEmbed -> ObeservableEmbed

--- packages/host-observable/src/index.tsx
+++ packages/host-observable/src/index.tsx
@@ -15,98 +15,6 @@ export { React };
 registerAllTheTools();
 
 type ObservableEmbedProps = {
-  localStorageKey: string,
-  reportOutputState?: (outputState: PromiseState<ToolOutput>) => void,
-  reportOutputP?: (outputState: EngraftPromise<ToolOutput>) => void,
-  inputs: {[name: string]: any} | undefined,
-  hide?: boolean,
-}
-
-export const ObservableEmbed = memo(function ObservableEmbed(props: ObservableEmbedProps) {
-  const { localStorageKey, inputs = {}, reportOutputState, reportOutputP, hide = false } = props;
-
-  // turn inputs provided from Observable into varBindings
-  const varBindings = useMemo(() => {
-    let varBindings: {[id: string]: VarBinding} = {};
-    Object.entries(inputs || {}).forEach(([name, value]) => {
-      const id = `ID${name}000000`;
-      varBindings[id] = {var_: {id, label: name}, outputP: EngraftPromise.resolve({value: value})};
-    });
-    return varBindings;
-  }, [inputs]);
-
-  const [program, updateProgram] = useState<ToolProgram>(
-    programFromLocalStorage(localStorageKey) || slotWithCode(defaultCodeFromInputs(inputs))
-  );
-
-  useEffect(() => {
-    try {
-      window.localStorage.setItem(localStorageKey, JSON.stringify(program));
-    } catch (e) {
-      console.warn("error saving to local storage", e);
-    }
-  }, [program, localStorageKey])
-
-  const [outputP, setOutputP] = useState<EngraftPromise<ToolOutput>>(EngraftPromise.resolve({value: undefined}));
-
-  const myReportOutputP = useCallback((outputP: EngraftPromise<ToolOutput>) => {
-    // Report it to the parent
-    reportOutputP && reportOutputP(outputP);
-    // Save it to state, so we can display it
-    setOutputP(outputP);
-  }, [reportOutputP]);
-
-  return (
-    <div>
-      <ToolOutputBuffer
-        outputP={outputP}
-        renderValue={(value) => {
-          // TODO: copied from elsewhere
-          let maybeElement = value as object | null | undefined;
-          if (isObject(maybeElement) && isValidElement(maybeElement)) {
-            return <ErrorBoundary>{maybeElement}</ErrorBoundary>;
-          }
-          return <ObservableInspector value={value}/>;
-        }}
-      />
-      { !hide &&
-        <div style={{marginTop: 10}}>
-          <IsolateStyles>
-            <ToolWithView
-              program={program}
-              varBindings={varBindings}
-              updateProgram={updateProgram}
-              reportOutputP={myReportOutputP}
-              reportOutputState={reportOutputState}
-            />
-          </IsolateStyles>
-        </div>
-      }
-    </div>
-  );
-});
-
-// TODO: useEngraft does this too
-function defaultCodeFromInputs(inputs: {[name: string]: any}) {
-  const names = Object.keys(inputs);
-  const firstName: string | undefined = names[0];
-  if (firstName) {
-    return `ID${firstName}000000`;
-  } else {
-    return '';
-  }
-}
-
-function programFromLocalStorage(key: string) {
-  const programJson = window.localStorage.getItem(key)
-  if (programJson) {
-    return JSON.parse(programJson);
-  } else {
-    return undefined;
-  }
-}
-
-type ObservableExtEmbedProps = {
   reportOutputState?: (outputState: PromiseState<ToolOutput>) => void,
   reportOutputP?: (outputState: EngraftPromise<ToolOutput>) => void,
   inputs: {[name: string]: any} | undefined,
@@ -114,7 +22,7 @@ type ObservableExtEmbedProps = {
   order: number,
 }
 
-export const ObservableExtEmbed = memo(function ObservableExtEmbed(props: ObservableExtEmbedProps) {
+export const ObservableEmbed = memo(function ObservableEmbed(props: ObservableEmbedProps) {
   const {inputs = {}, reportOutputState, reportOutputP, programString, order} = props;
 
   // turn inputs provided from Observable into varBindings
@@ -192,5 +100,13 @@ export const ObservableExtEmbed = memo(function ObservableExtEmbed(props: Observ
   );
 });
 
-
-
+// TODO: useEngraft does this too
+function defaultCodeFromInputs(inputs: {[name: string]: any}) {
+  const names = Object.keys(inputs);
+  const firstName: string | undefined = names[0];
+  if (firstName) {
+    return `ID${firstName}000000`;
+  } else {
+    return '';
+  }
+}
