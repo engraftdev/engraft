bea5a3368432ffbf73955b67d5cd5d45030a3147
Joshua Horowitz <joshuah@alum.mit.edu>
2023-05-08 23:42:48 -0700

    tool-markdown

---
+++ packages/tool-markdown/src/index.tsx
@@ -0,0 +1,62 @@
+/// <reference path="./jsx-runtime.d.ts" />
+import { markdown } from "@codemirror/lang-markdown";
+import { FancyCodeEditor, hookFancyCodeEditor, refREDirect, referencesFancyCodeEditor } from "@engraft/codemirror-helpers";
+import { ComputeReferences, EngraftPromise, ProgramFactory, ToolProgram, ToolProps, ToolView, UseUpdateProxy, defineTool, hookMemo, hooks, memoizeProps } from "@engraft/toolkit";
+import { evaluate } from "@mdx-js/mdx";
+import * as runtime from "react/jsx-runtime";
+
+export type Program = {
+  toolName: 'markdown',
+  code: string,
+  subPrograms: {[id: string]: ToolProgram},
+}
+
+const programFactory: ProgramFactory<Program> = () => ({
+  toolName: 'markdown',
+  code: '',
+  subPrograms: {},
+});
+
+const computeReferences: ComputeReferences<Program> = (program) =>
+  referencesFancyCodeEditor(program.code, program.subPrograms);
+
+const run = memoizeProps(hooks((props: ToolProps<Program>) => {
+  const { program, varBindings } = props;
+
+  const { referenceValuesP, subResults } = hookFancyCodeEditor({ code: program.code, subPrograms: program.subPrograms, varBindings });
+
+  const mdxCode = hookMemo(() => {
+    return program.code.replaceAll(refREDirect, (ref) => `props.${ref}`);
+  }, [program.code]);
+
+  const mdxCompiledP = hookMemo(() => {
+    console.log('mdxCode', mdxCode);
+    return evaluate(mdxCode, {...runtime});
+  }, [mdxCode]);
+
+  const outputP = hookMemo(() => EngraftPromise.all(referenceValuesP, mdxCompiledP).then(([referenceValues, mdxCompiled]) => {
+    return {value: mdxCompiled.default(referenceValues)};
+  }), [mdxCompiledP, referenceValuesP]);
+
+  const view: ToolView<Program> = hookMemo(() => ({
+    render: ({updateProgram, autoFocus, onBlur}) =>
+      <UseUpdateProxy updater={updateProgram} children={programUP =>
+        <FancyCodeEditor
+          extensions={[markdown()]}
+          code={program.code}
+          codeUP={programUP.code}
+          subPrograms={program.subPrograms}
+          subProgramsUP={programUP.subPrograms}
+          varBindings={varBindings}
+          autoFocus={autoFocus}
+          subResults={subResults}
+          defaultCode=""
+          onBlur={onBlur}
+        />
+      } />
+    }), [program.code, program.subPrograms, subResults, varBindings]);
+
+  return {outputP, view};
+}));
+
+export default defineTool({ programFactory, computeReferences, run })
