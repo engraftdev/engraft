a49002e823acd9d6ccb96b33485f77c073aa78d1
ts <tejfsingh@gmail.com>
2023-05-25 17:32:06 -0700

    clean up tests

--- packages/host-python/tests/test_basic.py
+++ packages/host-python/tests/test_basic.py
@@ -10,7 +10,10 @@ import json
 def wikipedia_extractor(response):
     return engraft.run_engraft(response, "wikipedia.json", edit=False)
 
-def array_index_2(a):
+def mixed_type_array(a):
+    return engraft.run_engraft(a, "mixed_type.json", edit=False)
+
+def nd_array_elementwise_add(a):
     return engraft.run_engraft(a, "nd_array.json", edit=False)
     
 class MyTest(unittest.TestCase):
@@ -35,14 +38,21 @@ class MyTest(unittest.TestCase):
 
         title2 = pages_list[1]['title']
         title3 = pages_list[2]['title']
+        output = wikipedia_extractor(response)
+        expected = [title2, title3]
+        self.assertEqual(output, expected)
 
-        correct = [title2, title3]
-        self.assertEqual(wikipedia_extractor(response), correct)
+    def test_mixed_type_array(self):
+        input = [4, [1,2,3], np.array([4,5,6]), 3]
+        output = mixed_type_array(input).all()
+        expected = input[2].all()
+        self.assertEqual(output, expected)
 
-    def test_nd_array(self):
-        a = [4, [1,2,3], np.array([4,5,6]), 3]
-        b = array_index_2(a)
-        self.assertEqual(b.all(), a[2].all())
+    def test_nd_array_elementwise_add(self):
+        input = np.array([1,2,3])
+        output = nd_array_elementwise_add(input).all()
+        expected = (input + 4).all()
+        self.assertEqual(output, expected)
 
     def test_decoder(self):
         class CustomDecoder(json.JSONDecoder):
@@ -57,7 +67,8 @@ class MyTest(unittest.TestCase):
         json_string = '[4, [1, 2, 3], {"__type": "nd-array", "__value": [4, 5, 6]}, 3]'
         output = CustomDecoder().decode(json_string)
         output[2] = output[2].all()
-        self.assertEqual([4, [1, 2, 3], np.array([4,5,6]).all(), 3], output)
+        expected = [4, [1, 2, 3], np.array([4,5,6]).all(), 3]
+        self.assertEqual(output, expected)
 
     def test_encoder(self):
         class CustomEncoder(json.JSONEncoder):
@@ -66,9 +77,10 @@ class MyTest(unittest.TestCase):
                     return {"__type": "nd-array", "__value": obj.tolist()}
                 return json.JSONEncoder.default(self, obj)    
         
-        a = [4, [1,2,3], np.array([4,5,6]), 3]
-        b = json.dumps(a, cls=CustomEncoder)
-        self.assertEqual(b, '[4, [1, 2, 3], {"__type": "nd-array", "__value": [4, 5, 6]}, 3]')
+        input = [4, [1,2,3], np.array([4,5,6]), 3]
+        output = json.dumps(input, cls=CustomEncoder)
+        expected = '[4, [1, 2, 3], {"__type": "nd-array", "__value": [4, 5, 6]}, 3]'
+        self.assertEqual(output, expected)
 
 
 if __name__ == '__main__':
