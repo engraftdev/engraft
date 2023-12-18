ba13cd30cb7ef510c94c7b1369d1093fa8fcdf99
Joshua Horowitz <joshuah@alum.mit.edu>
2023-12-18 08:56:54 -0800

    remove out-of-date comment

--- packages/testing-setup/src/index.ts
+++ packages/testing-setup/src/index.ts
@@ -5,8 +5,6 @@ import * as TestingRefsFunc from "./testing-refs-func.js";
 export * as TestingKnownOutput from "./testing-known-output.js";
 export * as TestingRefsFunc from "./testing-refs-func.js";
 
-// TODO: rename this package; parallel to basic-setup
-
 const tools: Tool[] = [
   forgetP(TestingKnownOutput.tool),
   forgetP(TestingRefsFunc.tool),
