5bb850a7dceadde767a8dc576a805e6b5bf99eb0
Joshua Horowitz <joshuah@alum.mit.edu>
2023-12-24 19:01:55 -0500

    shared: useContextMenu stopPropagation

--- packages/shared/src/useContextMenu.tsx
+++ packages/shared/src/useContextMenu.tsx
@@ -14,6 +14,7 @@ export function useContextMenu(menuMaker: MenuMaker): UseContextMenu {
 
   const openMenu = useCallback((ev: React.MouseEvent) => {
     ev.preventDefault();
+    ev.stopPropagation();
     setPosition({x: ev.pageX, y: ev.pageY});
   }, []);
 
